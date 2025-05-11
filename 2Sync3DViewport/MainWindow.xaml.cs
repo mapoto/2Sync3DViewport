ab4eb19528c781a1c8f902c484a611c0421d20a8
Lucky Chandrautama <chandrautama@outlook.de>
2025-05-11 15:40:43 +0200

    Add project files.

---
+++ 2Sync3DViewport/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Media.Media3D;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+//using 2Sync3DViewport.ViewModel;
+namespace _2Sync3DViewport
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        //public ContentViewModel contentViewModel { get; set; }
+
+        public MainWindow()
+        {
+            //InitializeComponent();
+
+
+            //var leftCamera = ContentViewerlLeft.Camera;
+            //var rightCamera = ContentViewerlRight.Camera;
+
+            //// Listen for changes in the left camera and update the right camera
+            //leftCamera.Changed += (s, e) =>
+            //{
+            //    ContentViewerlRight.Camera.Position = leftCamera.Position;
+            //    ContentViewerlRight.Camera.LookDirection = leftCamera.LookDirection;
+            //    ContentViewerlRight.Camera.UpDirection = leftCamera.UpDirection;
+            //};
+
+            //// Optionally, synchronize the other way around if needed
+            //rightCamera.Changed += (s, e) =>
+            //{
+            //    ContentViewerlLeft.Camera.Position = rightCamera.Position;
+            //    ContentViewerlLeft.Camera.LookDirection = rightCamera.LookDirection;
+            //    ContentViewerlLeft.Camera.UpDirection = rightCamera.UpDirection;
+            //};
+
+        }
+
+    }
+}
---
+++ 2Sync3DViewport/View/ContentViewer.xaml.cs
@@ -0,0 +1,97 @@
+﻿using HelixToolkit.Wpf;
+using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Media.Media3D;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _2Sync3DViewport.View
+{
+    /// <summary>
+    /// Interaction logic for ContentViewer.xaml
+    /// </summary>
+    public partial class ContentViewer : UserControl, INotifyPropertyChanged
+    {
+        private string filePath;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string FilePath
+        {
+            get => filePath;
+            set
+            {
+                filePath = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("BoundedFilePath"));
+            }
+        }
+        public PerspectiveCamera Camera
+        {
+            get => viewPort3d.Camera as PerspectiveCamera;
+            set => viewPort3d.Camera = value;
+        }
+
+        public ContentViewer()
+        {
+            InitializeComponent();
+        }
+
+        private void OpenButton_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == true)
+            {
+                // Filter for 3D files
+                openFileDialog.Filter = "3D Files (*.obj)|*.obj|All Files (*.*)|*.*";
+                FilePath = openFileDialog.FileName;
+
+                ModelVisual3D device3D = new ModelVisual3D();
+                device3D.Content = Display3d(filePath);
+
+                // Add to view port
+                viewPort3d.Children.Add(device3D);
+
+            }
+
+        }
+
+        private Model3D Display3d(string model)
+        {
+            Model3D device = null;
+            try
+            {
+                //Adding a gesture here
+                viewPort3d.RotateGesture = new MouseGesture(MouseAction.LeftClick);
+
+                //Import 3D model file
+                ModelImporter import = new ModelImporter();
+
+                //Load the 3D model file
+                device = import.Load(model);
+            }
+            catch (Exception e)
+            {
+                // Handle exception in case can not find the 3D model file
+                MessageBox.Show("Exception Error : " + e.StackTrace);
+            }
+            return device;
+        }
+
+        private void viewPort3d_Drop(object sender, DragEventArgs e)
+        {
+            filePath = e.Data.ToString();
+        }
+    }
+}
